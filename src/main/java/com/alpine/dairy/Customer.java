2db0fb0e8a86dd482b4c85ce882a31f7c9fcc981
Novel Poudel <poudelnovel@gmail.com>
2025-10-20 22:45:30 -0400

    methods to customer class; error in import

--- src/main/java/com/alpine/dairy/Customer.java
+++ src/main/java/com/alpine/dairy/Customer.java
@@ -7,8 +7,30 @@ import jakarta.persistence.Id;
 @Entity
 public class Customer {
     
-    private @Id
-    @GeneratedValue Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
-    
+    private String phoneNumber;
+
+    Customer() {}
+    Customer(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() { return this.id; }
+    public String getName() { return this.name; }
+    public String getPhoneNumber() { return this.phoneNumber; }
+
+    public void setId( Long id ) { this.id = id; }
+    public void setName( String name ) { this.name = name; }
+    public void setPhoneNumber( String phoneNumber ) { this.phoneNumber = phoneNumber; }
+
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(!(o instanceof Customer)) return false;
+        
+    }
+
 }
